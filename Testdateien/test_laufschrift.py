b044d6c75f996be8c78a81a47bbdba20fb4713d1
42Swampy <amamtrash@gmail.com>
2019-05-25 14:17:56 +0200

    Testdateien ausgelagert

--- Testdateien/test_laufschrift.py
+++
@@ -1,7 +0,0 @@
-# Test Laufschrift
-eingabe = "abcdefghijklmnopqrstuvwxyz"
-laenge = len(eingabe)
-bereich = laenge - 19
-for i in range(0,bereich):
-	ausgabe_laufschrift = eingabe[i:(i+20)]
-	print ausgabe_laufschrift
